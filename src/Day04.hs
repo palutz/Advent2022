244248a9788129bd3250ea73d2138959ce4ca2ce
Steo <palutz@users.noreply.github.com>
2022-12-18 23:45:10 +0000

    day04 part2

--- src/Day04.hs
+++ src/Day04.hs
@@ -7,7 +7,6 @@ dataFile04 = "data/04_day04.txt"
 test04 :: String
 test04 = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8\n"
 
-
 type RMin = Int 
 type RMax = Int 
 
@@ -16,18 +15,25 @@ data MyRange = MyRange {
   rmax :: RMax
 } deriving (Eq, Ord, Show)
 
+data Parts = Part1 | Part2
+
 class Contained a where 
   (|=|) :: a -> a -> Bool 
 
 instance Contained MyRange where 
   a |=| b = rmin a >= rmin b && rmax a <= rmax b
 
+class Overlapped a where 
+  (|~|) :: a -> a -> Bool 
+
+instance Overlapped MyRange where 
+  a |~| b = rmin b >= rmin a && rmin b <= rmax a
+
 splitAtChar :: Char -> String -> [String]
 splitAtChar c w = case dropWhile (== c) w of
                     "" -> []
                     s -> w1 : splitAtChar c s1
                           where (w1, s1) = break(== c) s
-
 
 listToTuple :: [a] -> (a, a)
 listToTuple [x1,x2] = (x1, x2)
@@ -50,9 +56,11 @@ rangeSplitted f s = listToMyRange $ f s
 contained :: [(MyRange, MyRange)] -> [(MyRange, MyRange)]
 contained = filter (\(l, r) -> (l |=| r) || (r |=| l))
 
+overlapped :: [(MyRange, MyRange)] -> [(MyRange, MyRange)]
+overlapped = filter (\(l, r) -> (l |~| r) || (r |~| l))
 
-part1 :: String -> Int
-part1 s = 
+workFlow :: String -> [(MyRange, MyRange)]
+workFlow s = 
   let
     ls :: [String]
     ls = lines s
@@ -60,13 +68,25 @@ part1 s =
     couples = map (splitAtChar ',') ls
     ranges :: [[MyRange]]
     ranges  = map ( map (rangeSplitted stringToSplittedInts)) couples
-    rts :: [(MyRange, MyRange)]
-    rts     = map listToTuple ranges  
   in 
-    length $ contained rts 
+    map listToTuple ranges  
+
+parts :: Parts -> String -> Int
+parts p s = length res 
+            where
+              res :: [(MyRange, MyRange)]
+              res = 
+                case p of 
+                  Part1 -> contained listJobs
+                  Part2 -> overlapped listJobs
+              listJobs = workFlow s 
 
 day04 :: FilePath -> IO () 
 day04 fp = 
   do 
     contents <- readFile fp
-    print $ part1 contents
+    let part1 = parts Part1 contents
+    let part2 = parts Part2 contents
+    print $ "Part1 = " <> show part1 <> ". Part2 = " <> show part2
+
+-- Part1 = 538. Part2 = 792
